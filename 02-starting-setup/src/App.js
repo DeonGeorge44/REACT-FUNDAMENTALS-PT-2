9ce5d6f8a3c2d8e858bb7f431eb1360398f7812f
DeonGeorge44 <ddeongeorge44@gmail.com>
2024-08-14 19:13:38 +0530

    props-intro

--- 02-starting-setup/src/App.js
+++ 02-starting-setup/src/App.js
@@ -2,12 +2,20 @@ import ExpenseItem from './components/ExpenseItem';
 
 
 function App() {
-  document.getElementById('root');
+  const expenses = [
+    {id:'e1',title:'Car Insurance' , amount : 294.67 , date:new Date(2021,2,28)},
+    {id:'e2',title:'Services' , amount : 294.67 , date:new Date(2019,5,6)},
+    {id:'e3',title:'Sales' , amount : 294.67 , date:new Date(2023,12,15)},
+
+  ]
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <p>okey that's it</p>
-      <ExpenseItem></ExpenseItem>
+      <h2>Let's Get Started</h2>
+      <ExpenseItem title={expenses[0].title} amount ={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
+      <ExpenseItem title={expenses[1].title} amount ={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
+      <ExpenseItem title={expenses[2].title} amount ={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
+      
+
     </div>
   );
 }
