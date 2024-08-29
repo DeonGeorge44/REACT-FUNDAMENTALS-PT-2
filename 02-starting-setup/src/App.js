b3b762caae48790b54a22313f712530a09b4a54f
DeonGeorge44 <ddeongeorge44@gmail.com>
2024-08-29 19:33:04 +0530

    assignment-on-compoenents-&-props

--- 02-starting-setup/src/App.js
+++ 02-starting-setup/src/App.js
@@ -4,16 +4,39 @@ import ExpenseItem from './components/ExpenseItem';
 function App() {
   const expenses = [
     {id:'e1',title:'Car Insurance' , amount : 294.67 , date:new Date(2021,2,28)},
-    {id:'e2',title:'Services' , amount : 294.67 , date:new Date(2019,5,6)},
-    {id:'e3',title:'Sales' , amount : 294.67 , date:new Date(2023,12,15)},
+    {id:'e2',title:'Services' , amount : 158.79 , date:new Date(2019,5,6)},
+    {id:'e3',title:'Sales' , amount : 94.67 , date:new Date(2023,12,15)},
 
   ]
   return (
     <div>
       <h2>Let's Get Started</h2>
-      <ExpenseItem title={expenses[0].title} amount ={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
+      {/* <ExpenseItem title={expenses = expenses[0].title} amount ={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
       <ExpenseItem title={expenses[1].title} amount ={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount ={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
+      <ExpenseItem title={expenses[2].title} amount ={expenses[2].amount} date={expenses[2].date}></ExpenseItem> */}
+      
+      <ExpenseItem
+      title={expenses[0].title}
+    amount ={expenses[0].amount}
+     date = {expenses[0].date}
+      > 
+      </ExpenseItem>
+
+      
+      <ExpenseItem
+      title={expenses[1].title}
+    amount ={expenses[1].amount}
+     date = {expenses[1].date}
+      > 
+      </ExpenseItem>
+      
+      <ExpenseItem
+      title={expenses[2].title}
+    amount ={expenses[2].amount}
+     date = {expenses[2].date}
+      > 
+      </ExpenseItem>
+
       
 
     </div>
---
+++ 03-Component-Task/src/Components/Concepts.js
@@ -0,0 +1,19 @@
+import 
+
+function Concepts(props){
+  return(
+    <div>
+      <ul id="concepts">
+      
+        <li className="concept">
+          <img src={props.image} alt={props.title} />
+          <h2>{props.title}</h2>
+          <p>{props.description}</p>
+        </li>
+       
+      </ul>
+    </div>
+  )
+}
+
+export default Concepts;
